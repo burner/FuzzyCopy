4a52b6831c6d41a8f56bce38362d3b23f8b434b2
Robert burner Schadek <rburners@gmail.com>
2018-07-16 08:04:39 +0100

    canBeConvertedWithTo

--- source/fuzzycopy.d
+++ source/fuzzycopy.d
@@ -1,5 +1,39 @@
 module fuzzycopy;
 
+template canBeConvertedWithTo(F,T) {
+	import std.traits : isIntegral, isFloatingPoint, isArray;
+	import std.range : ElementEncodingType;
+
+	static if(isIntegral!F && isIntegral!T) {
+		enum canBeConvertedWithTo = true;
+	} else static if(isIntegral!F && isFloatingPoint!T) {
+		enum canBeConvertedWithTo = true;
+	} else static if(isFloatingPoint!F && isIntegral!T) {
+		enum canBeConvertedWithTo = true;
+	} else static if(isArray!F && isArray!T) {
+		alias FType = ElementEncodingType!F;
+		alias TType = ElementEncodingType!T;
+		enum canBeConvertedWithTo = canBeConvertedWithTo!(FType,TType);
+	} else {
+		enum canBeConvertedWithTo = false;
+	}
+}
+
+unittest {
+	static assert(canBeConvertedWithTo!(int,byte));
+	static assert(canBeConvertedWithTo!(byte,float));
+	static assert(!canBeConvertedWithTo!(string,float));
+	static assert(!canBeConvertedWithTo!(int[],float));
+	static assert(canBeConvertedWithTo!(int[],float[]));
+	static assert(canBeConvertedWithTo!(byte[],double[]));
+	static assert(canBeConvertedWithTo!(double[],ubyte[]));
+}
+
+enum FuzzyCP {
+	Strict,
+	Fuzzy
+}
+
 void fuzzyCP(F,T)(ref F from, ref T to) {
 	import std.traits : hasMember, isImplicitlyConvertible;
 	foreach(mem; __traits(allMembers, F)) {
@@ -9,8 +43,12 @@ void fuzzyCP(F,T)(ref F from, ref T to) {
 		static if(hasMember!(T, mem)) {
 			alias ToType = typeof(__traits(getMember, to, mem));
 
+			// recursive if it is a struct
 			static if(is(FromType == struct) && is(ToType == struct)) {
-				fuzzyCP(__traits(getMember, from, mem), __traits(getMember, to, mem));
+				fuzzyCP(__traits(getMember, from, mem), 
+						__traits(getMember, to, mem)
+					);
+			// assign if it is assignable
 			} else  static if(isImplicitlyConvertible!(FromType,ToType)) {
 				__traits(getMember, to, mem) = __traits(getMember, from, mem);
 			}
